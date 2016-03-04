a0f572ddecb3a61f1dd5e5c91eb9487727fcea3b
jedielson <jedi_matind@hotmail.com>
2016-03-04 17:02:14 -0300

    commit inicial

---
+++ IdentityStudy.Api/Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Http;
+using IdentityStudy.Api.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly:OwinStartup(typeof(IdentityStudy.Api.Startup))]
+namespace IdentityStudy.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            ConfigureOAuth(app);
+
+            
+            WebApiConfig.Register(config);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions oAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(oAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            using (AuthRepository repo = new AuthRepository())
+            {
+                IdentityUser user = await repo.FindUser(context.UserName, context.Password);
+
+                if (user == null)
+                {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+
+        }
+    }
+}
---
+++ IdentityStudy.Spa/app/services/authService.js
@@ -0,0 +1,74 @@
+﻿'use strict';
+app.factory('authService', ['$http', '$q', 'localStorageService', function ($http, $q, localStorageService) {
+
+    //TODO: Js - Alterar a url base da api
+    var _serviceBase = 'http://ngauthenticationapi.azurewebsites.net/';
+    var _authServiceFactory = {};
+
+    var _authentication = {
+        isAuth: false,
+        userName: ""
+    };
+
+    var _saveRegistration = function (registration) {
+
+        _logOut();
+
+        return $http.post(_serviceBase + 'api/account/register', registration).then(function (response) {
+            return response;
+        });
+
+    };
+
+    var _login = function (loginData) {
+
+        var data = "grant_type=password&username=" + loginData.userName + "&password=" + loginData.password;
+
+        var deferred = $q.defer();
+
+        $http.post(_serviceBase + 'token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+            .success(function (response) {
+
+                localStorageService.set('authorizationData', { token: response.access_token, userName: loginData.userName });
+
+                _authentication.isAuth = true;
+                _authentication.userName = loginData.userName;
+
+                deferred.resolve(response);
+
+            }).error(function (err, status) {
+                _logOut();
+                deferred.reject(err);
+            });
+
+        return deferred.promise;
+
+    };
+
+    var _logOut = function () {
+
+        localStorageService.remove('authorizationData');
+
+        _authentication.isAuth = false;
+        _authentication.userName = "";
+
+    };
+
+    var _fillAuthData = function () {
+
+        var authData = localStorageService.get('authorizationData');
+        if (authData) {
+            _authentication.isAuth = true;
+            _authentication.userName = authData.userName;
+        }
+
+    }
+
+    _authServiceFactory.saveRegistration = _saveRegistration;
+    _authServiceFactory.login = _login;
+    _authServiceFactory.logOut = _logOut;
+    _authServiceFactory.fillAuthData = _fillAuthData;
+    _authServiceFactory.authentication = _authentication;
+
+    return _authServiceFactory;
+}]);
